63ca6dcfc99c7c3d350378cb9241ee8a0140df83
HugoLabatut <hugo.labatut24@protonmail.com>
2023-09-18 15:37:24 +0200

    tree: add some folder (include and class)

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil - Site location</title>
 </head>
+
 <body>
     <p>Hello, world!</p>
+    <?php echo "Hello World!"; ?>
 </body>
+
 </html>
