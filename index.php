efe1c6761c23552bc61b03a2100df6b726577188
HugoLabatut <hugo.labatut24@protonmail.com>
2023-10-09 16:41:19 +0200

    Ajout projet-locations.sql

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<!--
+    ===========================================
+    =========== SIO SITE LOCATION =============
+    ====== Date de début : 14 sept. 2023 ======
+    ===== Développement Front-end : CVt =======
+    ====== Développement Back-end : HLt =======
+    ========= Développement BDD : EQy =========
+    ===========================================
+-->
+
 <!DOCTYPE html>
 <html lang="en">
 
