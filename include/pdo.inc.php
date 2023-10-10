7a10cbf9f4fb507269e1a7a55caadcdd84612f08
HugoLabatut <hugo.labatut24@protonmail.com>
2023-10-10 08:33:16 +0200

    Initialisation

---
+++ include/pdo.inc.php
@@ -0,0 +1,13 @@
+<!-- 
+========= Site location
+========= pdo.inc.php
+========= Connexion à la base de données
+========= Date création : 10 oct. 2023
+========= Créateur : HLt
+-->
+
+<?php
+
+$con = new PDO("mysql:server=localhost;dbname=projet-locations", 'root', '');
+
+?>
---
+++ pages/clientlist.pages.php
@@ -0,0 +1,23 @@
+<!-- 
+========= Site location
+========= clientlist.pages.php
+========= Affiche les clients inscrits sur le site
+========= Date création : 10 oct. 2023
+========= Créateur : HLt
+-->
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Liste des clients - Logérance</title>
+</head>
+
+<body>
+    
+</body>
+
+</html>
