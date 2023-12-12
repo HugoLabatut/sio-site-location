15b3b4e38219c15e7fa83a33d88a2a0178875d84
HugoLabatut <hugo.labatut24@protonmail.com>
2023-12-12 11:26:24 +0100

    CRUD Bien de nouveau fonctionnel
    Correction erreur de syntaxe dans le CRUD des biens.

--- php/bien.traitement.php
+++ php/bien.traitement.php
@@ -13,21 +13,27 @@ require_once("../class/photo.class.php");
 
 if (isset($_POST['update'])) {
     $idb = $_POST['update'];
-    $nomb = $_POST['nombien'];
-    $rueb = $_POST['ruebien'];
-    $cpb = $_POST['cpbien'];
-    $vilb = $_POST['vilbien'];
-    $supb = $_POST['supbien'];
-    $coub = $_POST['coubien'];
-    $chab = $_POST['chabien'];
-    $desb = $_POST['desbien'];
-    $refb = $_POST['refbien'];
-    $statb = $_POST['statbien'];
-    $typeb = $_POST['typebien'];
+    $nomb = $_POST["nombien"];
+    $rueb = $_POST["ruebien"];
+    $idc = $_POST["idcommune"];
+    $supb = $_POST["supbien"];
+    $coub = $_POST["coubien"];
+    $chab = $_POST["chabien"];
+    $desb = $_POST["desbien"];
+    $refb = $_POST["refbien"];
+    $statb = $_POST["statbien"];
+    $idtb = $_POST["idtbien"];
+
+    var_dump($idb, $nomb, $rueb, $idc, $supb, $coub, $chab, $desb, $refb, $statb, $idtb);
 
     $oBien = new Bien($con);
 
-    $oBien->update($idb, $nomb, $rueb, $cpb, $vilb, $supb, $coub, $chab, $desb, $refb, $statb, $typeb);
+    if ($statb == "libre") {
+        $oBien->update($idb, $nomb, $rueb, $idc, $supb, $coub, $chab, $desb, $refb, '0', $idtb);
+    } else {
+        $oBien->update($idb, $nomb, $rueb, $idc, $supb, $coub, $chab, $desb, $refb, '1', $idtb);
+    }
+    header('location:../pages/biens.pages.php');
 } elseif (isset($_POST['update_photo'])) {
     $idb = $_POST['update_photo'];
     $unePhotoNom = $_FILES['photobien']['name'];
@@ -38,7 +44,7 @@ if (isset($_POST['update'])) {
     $lienPhoto = "../res/img/" . $unePhotoNom;
     $oPhoto = new Photo($con);
     $oPhoto->insert($unePhotoNom, $lienPhoto, $idb);
-    header('location:../pages/unbien.pages.php');
+    header('location:../pages/biens.pages.php');
 }
 
 ?>
